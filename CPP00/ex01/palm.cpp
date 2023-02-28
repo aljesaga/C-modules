7989439fe68d3509498bc6b201a3670ebb4b8acf
Alessandro jesus Sanchez gamberini <alsanche@c3r12s2.42madrid.com>
2023-02-28 18:14:25 +0100

    casi

--- CPP00/ex01/palm.cpp
+++ CPP00/ex01/palm.cpp
@@ -6,7 +6,7 @@
 /*   By: alsanche <alsanche@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 15:15:10 by alsanche          #+#    #+#             */
-/*   Updated: 2023/02/27 18:11:04 by alsanche         ###   ########lyon.fr   */
+/*   Updated: 2023/02/28 18:12:17 by alsanche         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,43 +28,64 @@ void	Phonebook::add_contact(int n_con)
 	std::string text[5] = {"first Name","last Name","Nickname","Phone Number","Darkest Secret"};
 	int	x, i;
 
-	if (n_con > MAX_C)
+	if (n_con >= MAX_C)
 		i = this->n_contacts % MAX_C;
 	else
 		i = n_con;
 	for(x=0; x < 5; x++)
 	{
 		std::cout<<"type "<<text[x]<<"for the contact\n-> ";
-		if (!std::getline(std::cin, aux))
+		if (std::getline(std::cin, aux))
+			this->contacts[i].add_value(x, aux);
+		else
 		{
 			std::cout<<"please type valid"<<text[x]<<std::endl;
 			x--;
 		}
-		else
-			this->contacts[i].add_value(x, aux);
 	}
 }
 
 void	Phonebook::view_palm()
 {
 	std::string	text;
-	int	i, x;
+	std::string title[5] = {"first Name","last Name","Nickname","Phone Number","Darkest Secret"};
+	int	i, x, n_con;
 
+	if (n_contacts >= MAX_C)
+		n_con = this->n_contacts % MAX_C;
+	else
+		n_con = this->n_contacts;
 	std::cout<<" ___________________________________________ \n";
 	std::cout<<"|Number--ID|First-Name|Last--Name|-Nickname-|\n";
-	for(i=0; i < n_contacts; i++)
+	std::cout<<"|-------------------------------------------|\n";
+	for(i=0; i < n_con; i++)
 	{
 		if (i >= 1)
 			std::cout<<"|----------|----------|----------|----------|\n";
-		std::cout<<"|"<<std::setfill(' ')<<std::setw(10)<<i<<"|";
+		std::cout<<"|"<<std::setfill(' ')<<std::setw(10)<<i + 1<<"|";
 		for (x = 0; x < 3; x++)
 		{
 			text = this->contacts[i].get_value(x);
-			std::cout<<std::setfill(' ')<<std::setw(10)<<text<<"|";
+			if (text.size() > 10)
+				std::cout<<std::setfill(' ')<<std::setw(9)<<text.substr(0,9)<<".|";
+			else
+				std::cout<<std::setfill(' ')<<std::setw(10)<<text<<"|";
 		}
 		std::cout<<std::endl;
 	}
 	std::cout<<"|__________|__________|__________|__________|\n";
+	std::cout<<"type ID to see all info :: ";
+	std::cin.;
+	if (i >= 1 && i <= n_con)
+	{
+		for(x = 0; x < 5; x++)
+		{
+			text = this->contacts[i - 1].get_value(x);
+			std::cout<<"\033[32m"<<title[x]<<"\033[0m [ "<<text<<" ]\n";
+		}
+	}
+	else
+		std::cout<<"invalid ID"<<std::endl;
 }
 
 int	Phonebook::init_palm()
@@ -73,7 +94,7 @@ int	Phonebook::init_palm()
 
 	while(1)
 	{
-		std::cout<<"Please enter a command->: ";
+		std::cout<<"\033[0mPlease enter a command->: ";
 		if (std::getline(std::cin, aux))
 		{
 			if (aux == "ADD")
