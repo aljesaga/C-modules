0a9e3cdffbdfa09aa0d011895dbfb15a931488cd
Alessandro jesus Sanchez gamberini <alsanche@c1r13s2.42madrid.com>
2023-05-26 17:49:48 +0200

    correccion de textos

--- CPP01/ex04/Mixer.cpp
+++ CPP01/ex04/Mixer.cpp
@@ -1,4 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Mixer.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alsanche <alsanche@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/26 17:38:27 by alsanche          #+#    #+#             */
+/*   Updated: 2023/05/26 17:38:28 by alsanche         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Mixer.hpp"
+
+void Mixer::get_text()
+{
+    size_t start, end, pos;
+    std::string _line;
+    
+    if (this->Error == 0)
+    {
+        while (std::getline(this->in, _line))
+        {
+            start = _line.find(this->_text1);
+            if (start != std::string::npos)
+            { 
+                end = this->_text1.size();
+                _line.erase(start, end);
+                if(start >= end)
+                    pos = start - end;
+                end = this->_text2.size();
+                _line.insert(start, this->_text2);
+                this->out<<_line<<std::endl;
+            }
+            else
+                this->out<<_line<<std::endl;
+        }
+    }
+}
 
 Mixer::Mixer(std::string Fd, std::string T1, std::string T2)
 {
@@ -12,48 +50,23 @@ Mixer::Mixer(std::string Fd, std::string T1, std::string T2)
         std::cout<<"Empty string"<<std::endl;
         this->Error = 1;
     }
-    this->_out_file = this->_filename.append("_modified");
-    this->out.open(this->_out_file);
+    this->_out_file = this->_filename;
+    this->_out_file.append("_mixed");
     this->in.open(this->_filename);
-    if (this->in.fail() || this->out.fail())
+    if (this->in.fail())
     {
-        std::cout<<"file dont open"<<std::endl;
+        std::cout<<"In file dont open"<<std::endl;
         this->Error = 1;
     }
-    return ;
+    this->out.open(this->_out_file);
+    if (this->out.fail())
+    {
+        std::cout<<"Out file dont open"<<std::endl;
+        this->Error = 1;
+    }
 }
 Mixer::~Mixer()
 {
-    this->in.close();
-    this->out.close();
-    return ;
-}
-
-void Mixer::get_text()
-{
-    size_t start, end, pos;
-    std::string _line;
-    
-    if (this->Error == 0)
-    {
-        std::getline(this->in,_line);
-        std::cout<<_line<<std::endl;
-        while (_line.size() != 0)
-        {
-            start = _line.find(this->_text1);
-            if (start >= 0)
-            { 
-                end = this->_text1.size();
-                _line.erase(start, end);
-                if(start >= end)
-                    pos = start - end;
-                end = this->_text2.size();
-                _line.insert(start, this->_text2);
-                this->out<<_line<<std::endl;
-            }
-            else
-                this->out<<_line<<std::endl;
-            std::getline(this->in, _line);
-        }
-    }
+        this->in.close();
+        this->out.close();
 }
--- CPP01/ex05/Half.hpp
+++ CPP01/ex05/Half.hpp
@@ -1,39 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Mixer.hpp                                          :+:      :+:    :+:   */
+/*   Half.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alsanche <alsanche@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/26 17:38:31 by alsanche          #+#    #+#             */
-/*   Updated: 2023/05/26 17:38:32 by alsanche         ###   ########lyon.fr   */
+/*   Created: 2023/05/26 17:38:11 by alsanche          #+#    #+#             */
+/*   Updated: 2023/05/26 17:45:47 by alsanche         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MIXER_HPP
-# define MIXER_HPP
+#ifndef HARL_HPP
+# define HARL_HPP
 
-#include <iostream>
-#include <fstream>
+# include <iostream>
 
-class Mixer
+class Harl
 {
     private:
-
-        std::ifstream in;
-        std::ofstream out;
-        std::string _text1;
-        std::string _text2;
-        std::string _filename;
-        std::string _out_file;
-        int         Error; 
-
+    
+        void    debug(void);
+        void    info(void);
+        void    warning(void);
+        void    error(void);
+    
     public:
-        
-        void    get_text();
 
-        Mixer(std::string Fd, std::string T1, std::string T2);
-        ~Mixer();
+        void    complain(std::string level);
 
 };
 
