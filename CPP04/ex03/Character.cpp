79da31dc3eba020d181338ef8bcf62fc9007e075
alsanche <alejesaga1@gmail.com>
2023-09-07 13:40:56 +0200

    Character.cpp_complit

--- CPP04/ex03/Character.cpp
+++ CPP04/ex03/Character.cpp
@@ -40,15 +40,47 @@ std::string const & Character::getName() const
 void Character::equip(AMateria *m)
 {
     int i = -1;
-    
-    while(++i < 4)
+    if (m)
     {
-        if (this->slots[i] == NULL)
+        while(++i < 4)
         {
-            slots[i] = m;
-            std::cout<<this->getName()<<" Equip materia "<<m->getType()<<" in slots "<<i<<std::endl;
-            break ;
+            if (this->slots[i] == NULL)
+            {
+                slots[i] = m;
+                std::cout<<this->getName()<<":: Equipped materia "<<m->getType()<<" in slots "<<i<<std::endl;
+                break ;
+            }
         }
+        if (i == 4)
+            std::cout<<this->getName()<<":: Inventory is full"<<std::endl;
     }
 }
 
+void    Character::unequip(int idx)
+{
+    if (idx >= 0 && idx <= 3)
+    {
+        if (this->slots[idx] != NULL)
+        {    
+            std::cout<<this->getName()<<":: Unequipped materia "<<this->slots[idx]->getType()<<end
+            this->slots[idx] = NULL;
+        }
+        else
+            std::cout<<this->getName()<<":: This slots is empty"<<std::endl;
+    }
+}
+
+void    Character::use(ind idx, ICharacter &target)
+{
+    if (this->slots[idx] != NULL)
+    {
+        this->slots[idx].use(target);
+    }
+}
+
+Character& Character::operator=(Character const & cpy)
+{
+    if (&this != &cpy)
+        *this = cpy;
+    return (*this);
+}
